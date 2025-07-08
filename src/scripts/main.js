fe2c72b383c2697fa430a5815467c915835a9c2f
Artur Shatskov <artur.shackov77@gmail.com>
2025-07-08 21:47:27 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,64 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick;
+  let rightClick;
+
+  document.addEventListener('click', () => {
+    leftClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', () => {
+    rightClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+function success(message) {
+  const div = document.createElement('div');
+
+  div.className = 'success';
+  div.dataset.qa = 'notification';
+  div.innerText = message;
+
+  document.body.appendChild(div);
+}
+
+function error(message) {
+  const div = document.createElement('div');
+
+  div.className = 'error';
+  div.dataset.qa = 'notification';
+  div.innerText = message;
+
+  document.body.appendChild(div);
+}
+
+firstPromise.then(success).catch(error);
+secondPromise.then(success);
+thirdPromise.then(success);
